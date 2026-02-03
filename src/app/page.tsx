b2ce3497c7eed754143ec924e288fba996c05841
luksu08 <49532158+luksu08@users.noreply.github.com>
2026-02-03 19:23:31 +0000

    build test stamp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,3 +3,7 @@ import { redirect } from "next/navigation";
 export default function Home() {
   redirect("/login");
 }
+
+<div style={{padding: 12, background: "yellow", color: "black", fontWeight: 700}}>
+  BUILD TEST: 2026-02-03 12:45
+</div>
